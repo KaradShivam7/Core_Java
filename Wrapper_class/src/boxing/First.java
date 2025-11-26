2bab84becc81f50aaba5108e5d11f879a1a78e7d
KaradShivam7 <shivamkarad20@gmail.com>
2025-11-26 23:10:24 +0530

    Wrapper class programs

---
+++ Wrapper_class/src/boxing/First.java
@@ -0,0 +1,18 @@
+package boxing;
+
+public class First {
+
+	public static void main(String[] args) {
+		int i=10;
+		String str=String.valueOf(i);
+		System.out.println("string = "+ str);
+		
+		System.out.println("********************************************");
+		
+		float f=12.33f;
+		String str1= String.valueOf(f);
+		System.out.println("string = "+str1);
+		
+	}
+
+}
---
+++ Wrapper_class/src/unboxing/First.java
@@ -0,0 +1,17 @@
+package unboxing;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		String str="10";
+		int i=Integer.parseInt(str);
+		System.out.println("integer =" + i);
+		
+		System.out.println("*******************************************");
+		String str1="234.4";
+		double d=Double.parseDouble(str1);
+		System.out.println("Double = "+d);
+	}
+
+}
