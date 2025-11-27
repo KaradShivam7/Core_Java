eb96a2ac6d18d27dd41535a40eec128c9b76fabb
KaradShivam7 <shivamkarad20@gmail.com>
2025-11-27 21:48:11 +0530

    map()

--- Stream_API/src/Intermediatary/map.java
+++ Stream_API/src/Intermediatary/map.java
@@ -2,6 +2,7 @@ package Intermediatary;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class map {
 
---
+++ Stream_API/src/Intermediatary/map_1.java
@@ -0,0 +1,10 @@
+package Intermediatary;
+
+public class map_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
