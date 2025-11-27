379201d08af254ca9b767f30237d4b8d16b377a0
KaradShivam7 <shivamkarad20@gmail.com>
2025-11-27 20:55:48 +0530

    filter()

---
+++ Stream_API/src/Intermediatary/fillter.java
@@ -0,0 +1,24 @@
+package Intermediatary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class fillter {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list=new ArrayList<Integer>();
+		
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		
+		System.out.println(list);
+		
+		System.out.println("***************************");
+		
+		list.stream().filter((i)->{return i<=50;}).forEach((i)->{System.out.println(i);});
+	}
+
+}
