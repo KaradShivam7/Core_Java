5b9fc89a84d9527735519ec78f89d91b84f62805
KaradShivam7 <shivamkarad20@gmail.com>
2025-11-27 21:31:43 +0530

    map()

---
+++ Stream_API/src/Intermediatary/map.java
@@ -0,0 +1,20 @@
+package Intermediatary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class map {
+
+	public static void main(String[] args) {
+		List<Integer> list =new ArrayList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		
+		System.out.println(list);
+		System.out.println("**************************");
+		
+		list.stream().map((i)->{return Double.valueOf(i);}).filter((i)->{return i<=30;}).forEach((i)->{System.out.println(i*2);});
+	}
+
+}
