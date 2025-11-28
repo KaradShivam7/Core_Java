a2ec3d4beb5f6f9f9200304f05acbee2e777e60a
KaradShivam7 <shivamkarad20@gmail.com>
2025-11-28 21:31:19 +0530

    List<List<Integer>>

--- Stream_API/src/Intermediatary/map_1.java
+++ Stream_API/src/Intermediatary/map_1.java
@@ -1,10 +1,35 @@
 package Intermediatary;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class map_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> list =new ArrayList<Integer>();
+		list.add(10);
+		list.add(20);
+		
+		List<Integer> list1 =new ArrayList<Integer>();
+		list1.add(30);
+		list1.add(40);
+		
+//		List<Integer> list2 =new ArrayList<Integer>();
+//		list2.add(50);
+//		list2.add(60);
+//		
+//		List<Integer> list3 =new ArrayList<Integer>();
+//		list3.add(70);
+//		list3.add(80);
+		
+		List<List<Integer>> list4 =new ArrayList<List<Integer>>();
+		list4.add(list);
+		list4.add(list1);
+		
+		System.out.println(list4);
+		
+		list4.stream().forEach((i)->{i.stream().forEach((j)->{System.out.println(j*2);});});
+		
 	}
 
 }
