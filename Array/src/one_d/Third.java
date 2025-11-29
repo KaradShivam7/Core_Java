e892f39260b1cb13d2b2f268d126f69099cf51d5
KaradShivam7 <shivamkarad20@gmail.com>
2025-11-29 23:48:57 +0530

    array using scanner

---
+++ Array/src/one_d/Third.java
@@ -0,0 +1,36 @@
+package one_d;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Third {
+
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("enter size of array");
+			int n=sc.nextInt();
+			
+			int arr[]=new int[n];
+			
+			System.out.println("enter elements of array");
+			for(int i=0; i<n; i++) {
+				arr[i]=sc.nextInt();
+			}
+			
+			System.out.println(Arrays.toString(arr));
+			System.out.println("********************");
+			
+			System.out.println("array elements are");
+			for(int i=0;i<n;i++) {
+				System.out.print(arr[i]);
+				System.out.print(" ");
+			}
+			
+			
+			
+			sc.close();
+
+		}
+
+	}
+
