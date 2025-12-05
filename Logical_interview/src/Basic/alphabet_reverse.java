cfb1f78bacfb55c198309c76cb5004bd27a9f60c
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-05 18:14:55 +0530

    alphabet reverse

---
+++ Logical_interview/src/Basic/alphabet_reverse.java
@@ -0,0 +1,13 @@
+package Basic;
+
+class alphabet_reverse
+{
+  public static void main(String[] args)
+	{
+          for(char ch='z';ch>='a'; ch--)
+             {
+              
+              System.out.println(ch);
+	     }
+	}
+}
