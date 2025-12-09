f3b816fdc4c1d686748c2bc42b5a8468de7b0fc9
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-09 22:04:54 +0530

    natural number

---
+++ Logical_interview/src/Basic/naturalno.java
@@ -0,0 +1,17 @@
+package Basic;
+
+class naturalno
+{
+  public static void main(String[] args)
+	{
+          int i;
+          for(i=1;i<=10;i++)
+             {
+		if(i==5)
+		  {
+                    continue;
+		  }
+               System.out.println(i);
+             }
+	}
+}
