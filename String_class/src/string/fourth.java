158fcf044d139f82fa16e69c746bdc86f0d23af1
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-11 21:12:00 +0530

    string

---
+++ String_class/src/string/fourth.java
@@ -0,0 +1,15 @@
+package string;
+
+public class fourth {
+
+	public static void main(String[] args) {
+		String s="shiv";
+		System.out.println(s.toUpperCase());
+		
+		System.out.println("**********");
+		
+		String s1="KARAD";
+		System.out.println(s1.toLowerCase());
+	}
+
+}
