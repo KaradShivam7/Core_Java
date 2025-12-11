aee600b4691ffe0efc12430ebefb30d54cb135ce
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-11 21:11:19 +0530

    string

---
+++ String_class/src/string/second.java
@@ -0,0 +1,11 @@
+package string;
+
+public class second {
+
+	public static void main(String[] args) {
+		String str= new String("shiv");          //String object
+		System.out.println(str);
+	}
+
+}
+
