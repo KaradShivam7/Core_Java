22e5f47d4d75450ddbe7758448b64fd92e73802e
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-11 21:13:00 +0530

    string

---
+++ String_class/src/string/seventh.java
@@ -0,0 +1,25 @@
+package string;
+
+public class seventh {
+
+	public static void main(String[] args) {
+		String str="Karad";
+		String str1 ="Shivam";
+		
+		System.out.println(str.length());
+		System.out.println(str1.length());
+		
+		System.out.println("***********");
+		
+		System.out.println(str.contains("ar"));
+		System.out.println(str1.contains("va"));
+		
+		System.out.println("*******************");
+		
+		System.out.println(str.lastIndexOf('a'));
+		System.out.println(str1.indexOf('m'));
+		
+		
+	}
+
+}
