faaf2a56220ec9b3ccd52a37517e93803b7b6926
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-11 21:14:04 +0530

    string

---
+++ String_class/src/string/tenth.java
@@ -0,0 +1,17 @@
+package string;
+
+public class tenth {
+
+	public static void main(String[] args) {
+		String str=new String("Shivam");
+		String s="";
+		String s1="     ";
+		System.out.println(str);
+		System.out.println("******************");
+		
+		System.out.println(str.isEmpty());
+		System.out.println(s.isEmpty());
+		System.out.println(s1.isBlank());
+	}
+
+}
