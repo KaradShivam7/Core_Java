4abebbc49bbaccbad6a0004076a1efce16e5fb9d
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-11 21:11:39 +0530

    string

---
+++ String_class/src/string/third.java
@@ -0,0 +1,12 @@
+package string;
+
+public class third {
+
+	public static void main(String[] args) {
+		String s="shiv";
+		s=s+" karad";   //creates new string
+		
+		System.out.println(s);
+	}
+
+}
