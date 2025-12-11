4a89816b695a67bf05f652a99b60f8adad7bf87b
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-11 21:10:10 +0530

    string buffer

---
+++ String_class/src/stringbuffer/Third.java
@@ -0,0 +1,18 @@
+package stringbuffer;
+
+public class Third {
+
+	public static void main(String[] args) {
+		StringBuffer sb=new StringBuffer("Shivam");
+		System.out.println(sb);
+		System.out.println("***********************");
+		
+//		System.out.println(sb.delete(1, 3));
+//		System.out.println(sb.deleteCharAt(2));
+//		
+//		System.out.println("****************");
+		
+		System.out.println(sb.replace(4, 6, "kanya"));
+	}
+
+}
---
+++ String_class/src/stringbuffer/first.java
@@ -0,0 +1,13 @@
+package stringbuffer;
+
+public class first {
+
+	public static void main(String[] args) {
+		
+		StringBuffer s=new StringBuffer("shiv");
+		System.out.println(s);
+		
+	
+	}
+
+}
