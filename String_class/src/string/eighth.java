ece4257fb5c8f900612a53daf745eb8e7cd4c2c9
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-11 21:13:24 +0530

    string

---
+++ String_class/src/string/eighth.java
@@ -0,0 +1,23 @@
+package string;
+
+public class eighth {
+
+	public static void main(String[] args) {
+		String str="shivam";
+		System.out.println(str);
+		System.out.println("******************");
+		
+		System.out.println(str.endsWith("am"));
+		System.out.println(str.endsWith("s"));
+		
+		System.out.println("*******************");
+		
+		System.out.println(str.startsWith("shi"));
+		System.out.println(str.startsWith("i", 2));
+		
+		System.out.println("********************");
+		
+		System.out.println(str.intern());
+	}
+
+}
