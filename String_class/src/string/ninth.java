2c3172ae98f84c79cb8dabd2e94d195c4ec9da59
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-11 21:13:42 +0530

    string

---
+++ String_class/src/string/ninth.java
@@ -0,0 +1,14 @@
+package string;
+
+public class ninth {
+
+	public static void main(String[] args) {
+		String str="   Shivam      ";
+		System.out.println(str);
+		System.out.println("******************");
+		
+		System.out.println(str.trim());
+		
+	}
+
+}
