b897047e3be31f3d84b1060532b317534c13cca7
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-11 21:12:19 +0530

    string

---
+++ String_class/src/string/fifth.java
@@ -0,0 +1,12 @@
+package string;
+
+public class fifth {
+
+	public static void main(String[] args) {
+		String str="shivam";
+		System.out.println(str);
+		System.out.println("**********");
+		System.out.println(str.concat(" karad"));
+	}
+
+}
