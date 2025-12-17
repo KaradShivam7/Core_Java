cea954a84877b037f3c55c2b9e7f1c99e61cc26a
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-17 18:47:33 +0530

    java

---
+++ Logical_interview/src/mock_questions/move_zeroto_last.java
@@ -0,0 +1,22 @@
+package mock_questions;
+
+import java.util.Arrays;
+
+public class move_zeroto_last {
+
+	public static void main(String[] args) {
+		int arr[]= {1,0,2,0,3,0,4,0,0,8,0};
+		int arr1[]= new int[arr.length];
+		
+			int counter=0;
+			for(int j=0;j<arr.length;j++) {
+				if(arr[j]!=0) {
+					arr1[counter]=arr[j];
+					counter++;
+				}
+			}
+		
+		System.out.println(Arrays.toString(arr1));
+	}
+
+}
