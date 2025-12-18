6e8596e8063a6999c8a4aecf9c7585000c293191
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-18 22:04:04 +0530

    logical question

---
+++ Logical_interview/src/mock_questions/list_distinct.java
@@ -0,0 +1,15 @@
+package mock_questions;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class list_distinct {
+
+	public static void main(String[] args) {
+		List<String> list =Arrays.asList("asdf","asdfde","kjhgfd","edfgbnm","pj","oih","sd","ert");
+	 
+		list.stream().map(String::length).distinct().sorted((o1,o2)->(o2-o1)).forEach(System.out::println);
+	
+	}
+
+}
