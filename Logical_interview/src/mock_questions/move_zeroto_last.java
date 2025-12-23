f6502fe8b2e8ef5ed18318e6a4b02631b8a388f0
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-23 22:35:53 +0530

    move zero to last

--- Logical_interview/src/mock_questions/move_zeroto_last.java
+++ Logical_interview/src/mock_questions/move_zeroto_last.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class move_zeroto_last {
 
 	public static void main(String[] args) {
-		int arr[]= {1,0,2,0,3,0,4,0,0,8,0};
+		int arr[]= {1,0,2,0,3,0,4,0,0,8,0,9};
 		int arr1[]= new int[arr.length];
 		
 			int counter=0;
