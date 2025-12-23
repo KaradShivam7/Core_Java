85921305ae3b1ae129d5fb98a0e8406bd4c7650c
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-23 22:35:08 +0530

    static method :: reference

--- Stream_API/src/method_reference/static3.java
+++ Stream_API/src/method_reference/static3.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class static3 {
 
 	public static void main(String[] args) {
-		List<String> list =Arrays.asList("karad","kaka","karanataka","maharastra","latur");
+		List<String> list =Arrays.asList("karad","kaka","mama","karanataka","maharastra","latur");
 		
 		list.stream().filter(check::end)
 		.map(check::change)
