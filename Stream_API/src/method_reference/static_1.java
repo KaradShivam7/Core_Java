de8cfaa52450e318372e134c347798fc47cff30f
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-23 22:34:15 +0530

    static method :: reference

--- Stream_API/src/method_reference/static_1.java
+++ Stream_API/src/method_reference/static_1.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class static_1 {
 
 	public static void main(String[] args) {
-		List<Integer> list =Arrays.asList(10,20,33,11,53,67,84,20);
+		List<Integer> list =Arrays.asList(10,20,33,11,53,67,84,20,23);
 		
 		list.stream().filter(even::check).forEach(System.out::println);
 	}
