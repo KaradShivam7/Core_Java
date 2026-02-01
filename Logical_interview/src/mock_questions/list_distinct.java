2693e1f8669c78021a0a7fd9093f28799fb9775a
KaradShivam7 <shivamkarad20@gmail.com>
2026-02-01 20:20:28 +0530

    distinct length

--- Logical_interview/src/mock_questions/list_distinct.java
+++ Logical_interview/src/mock_questions/list_distinct.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class list_distinct {
 
 	public static void main(String[] args) {
-		List<String> list =Arrays.asList("asdf","asdfde","kjhgfd","edfgbnm","pj","oih","sd","ert");
+		List<String> list =Arrays.asList("asdf","asdfde","kjhgfd","edfgbnm","pj","oih","sd","ert","wedfcxaqwed");
 	 
 		list.stream().map(String::length).distinct().sorted((o1,o2)->(o2-o1)).forEach(System.out::println);
 	
