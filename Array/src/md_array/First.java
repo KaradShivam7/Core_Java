68a111e57d097d08fa8bd2af8b2d9bfbfbd27ea7
KaradShivam7 <shivamkarad20@gmail.com>
2026-03-17 20:24:31 +0530

    java

--- Array/src/md_array/First.java
+++ Array/src/md_array/First.java
@@ -2,7 +2,7 @@ package md_array;
 
 public class First {
 		public static void main(String[] args) {
-			int arr[][]= {{1,2,3,4,},{5,6,7,8},{9,10,11,12}};
+			int arr[][]= {{1,2,3,4,},{5,6,7,8},{9,10,11,12,13}};
 			
 			
 			for(int row=0;row<arr.length;row++) {
