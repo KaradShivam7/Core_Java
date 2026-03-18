77445244dfa8f3ce3219e6e06bb27849257ed821
KaradShivam7 <shivamkarad20@gmail.com>
2026-03-18 12:50:22 +0530

    java

--- inheritance/src/multiple/first.java
+++ inheritance/src/multiple/first.java
@@ -2,9 +2,11 @@ package multiple;
 
 public class first {
          int i;
+		 int j;
 	public static void main(String[] args) {
 		first f=new first();
 		System.out.println(f.i);
+		System.out.println(f.j);
 	}
 
 }
